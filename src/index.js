9550c0ac5206f4f2c4d48999e401a4b5718ef8ea
Fellum <fkekerman@gmail.com>
2023-09-01 15:04:38 +0400

    add cors for dev

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express, { json } from 'express'
+import cors from 'cors'
 import router from './presenters/express/routes/index.js'
 import logger from './presenters/express/middlewares/logger.js'
 import errorHandler from './presenters/express/middlewares/errorHandler.js'
+
 const app = express()
 
 app.use(json())
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 app.use(logger)
 app.use(router)
 app.use(errorHandler)
