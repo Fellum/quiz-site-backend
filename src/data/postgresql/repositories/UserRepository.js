eac476b53d02878bd1273588dadaee34d94d78b3
Fellum <fkekerman@gmail.com>
2023-09-02 15:09:10 +0400

    remove old code

--- src/data/postgresql/repositories/UserRepository.js
+++ src/data/postgresql/repositories/UserRepository.js
@@ -3,16 +3,9 @@ import PostgresRepository from './PostgresRepository.js'
 export default class UserRepository extends PostgresRepository {
   static tableName = 'users'
   static usernameEmailExists ({ username, email }) {
-    // const knex = this.getKnex()
-    // const usersQuery = this.getQueryBuilder()
-    //   .select(1)
-    //   .where({ email })
-    //   .orWhere({ username })
-    //   .limit(1)
     return this.getKnex()
       .select(this.views.withEmailExists(email))
       .select(this.views.withUsernameExists(username))
-      // .select(knex.raw('exists(??) as exists', [usersQuery]))
       .first()
   }
 
