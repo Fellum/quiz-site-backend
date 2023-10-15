74c8e361bc9c486cbf79d4dda549e36978c451ac
Fellum <fkekerman@gmail.com>
2023-10-16 02:40:42 +0400

    cookies fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const app = express()
 app.use(cookieParser())
 app.use(json())
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'http://localhost:3000',
+  credentials: true
 }))
 app.use(logger)
 app.use(router)
